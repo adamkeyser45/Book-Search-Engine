432a84ff6256c274d5475fe6ed1bf07446a42e45
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-25 14:11:02 -0500

    typeDefs completed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,49 @@
 const { gql } = require('apollo-server-express');
 
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    input bookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: bookInput): User
+        removeBook(bookId: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        SavedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
 
 
 module.exports = typeDefs;
